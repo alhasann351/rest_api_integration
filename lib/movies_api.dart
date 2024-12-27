48824b17a1d7fb3cf523473e05c68b5a0d747f53
alhasann351 <alhasann352@gmail.com>
2024-12-28 00:37:27 +0600

    staggered-grid-view-cached-network-image-package-use

--- lib/movies_api.dart
+++ lib/movies_api.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:http/http.dart' as http;
 
 import 'model/get_api_model.dart';
@@ -59,30 +61,61 @@ class _MoviesApiState extends State<MoviesApi> {
               ),
             );
           } else {
-            return GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            return MasonryGridView.count(
+                shrinkWrap: true,
+                cacheExtent: 1500,
                 crossAxisCount: 3,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.white,
+                    clipBehavior: Clip.antiAlias,
+                    elevation: 5,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        CachedNetworkImage(
+                          imageUrl: snapshot.data![index].posterPath.toString(),
+                        ),
+                        Text(
+                          snapshot.data![index].title.toString(),
+                        ),
+                        Text(
+                          snapshot.data![index].releaseDate.toString(),
+                        ),
+                      ],
+                    ),
+                  );
+                });
+            /*return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // Number of columns
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.7,
               ),
+              padding: EdgeInsets.all(10),
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
-                return Card(
-                  color: Colors.white,
-                  elevation: 5,
+                return Container(
                   child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      Expanded(
-                        child: Image.network(
-                          snapshot.data![index].posterPath.toString(),
-                          height: 100,
-                          fit: BoxFit.fill,
-                        ),
+                      Image.network(
+                        snapshot.data![index].posterPath.toString(),
+                        fit: BoxFit.cover,
+                        height: 100,
                       ),
-                      Text('Movies'),
+                      Text(
+                        snapshot.data![index].title.toString(),
+                      ),
                     ],
                   ),
                 );
               },
-            );
+            );*/
           }
         },
       ),
